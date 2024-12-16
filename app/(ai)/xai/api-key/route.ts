a0fb8cfefe71d34fe0420745b3e0d3ac4606dff3
Aiden Dong <byteappua@gmail.com>
2024-12-17 00:43:27 +0800

    1

--- app/(ai)/xai/api-key/route.ts
+++ app/(ai)/xai/api-key/route.ts
@@ -2,14 +2,17 @@ import { NextResponse } from "next/server";
 
 export async function GET() {
   const response = await fetch("https://api.x.ai/v1/api-key", {
+    method: "GET",
     headers: {
+      "Content-Type": "application/json",
+      Accept: "application/json",
       Authorization: `Bearer ` + process.env.XAI_API_KEY, // 替换为你的 OpenAI API 密钥
     },
   });
   if (!response.ok) {
     return NextResponse.error();
   }
-  return NextResponse.json(response.json);
+  return new NextResponse(response.body);
 }
 
 export const runtime = "edge";
